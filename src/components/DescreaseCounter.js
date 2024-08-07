319df7bfe03d877a786997a53300384cb87e0ff6
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2024-08-07 16:09:12 +0300

    connecting the component to the reducer

---
+++ src/components/DescreaseCounter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DescreaseCounter extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
